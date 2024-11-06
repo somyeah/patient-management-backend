3caf697675bbc46c89bacfba287ceb4a921240bd
Somya Jain <sjain29@usfca.edu>
2024-11-06 12:33:43 -0800

    Modify database details

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:mar211998@127.0.0.1/:5432/patient_mgmt_db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:mar211998@127.0.0.1/:5432/finnisample")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
