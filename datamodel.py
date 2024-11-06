d740d57c134f0aae738425c307f6b1470682ed07
Somya Jain <sjain29@usfca.edu>
2024-11-06 12:41:55 -0800

    Modify connection details

--- datamodel.py
+++ datamodel.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Patient(db.Model):
-    __tablename__ = 'patients'
+    __tablename__ = 'patient'
     
     patient_id = db.Column(db.Integer, primary_key=True)
     patient_name = db.Column(db.String(500), nullable=False)
