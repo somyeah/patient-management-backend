4d0b62101e3bc9ff2f125b850a991d3f88e06bbe
Somya Jain <sjain29@usfca.edu>
2024-11-06 15:50:52 -0800

    Troubleshooting

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-api.add_resource(PatientList, '/api/patients/')
-api.add_resource(PatientData, '/api/patients/<int:patient_id>/')
+api.add_resource(PatientList, '/api/patients')
+api.add_resource(PatientData, '/api/patients/<int:patient_id>')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
