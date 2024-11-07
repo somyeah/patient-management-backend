dfc612a95d62fd36277d05be50ed59af1130afbc
Somya Jain <sjain29@usfca.edu>
2024-11-06 19:44:29 -0800

    Add headers after a request?

--- app.py
+++ app.py
@@ -10,13 +10,20 @@ app.config.from_object(Config)
 api = Api(app)
 
 db.init_app(app)
-CORS(app, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], allow_headers=["Content-Type"], supports_credentials=True)
+CORS(app)
 
 with app.app_context():
     db.create_all()
 
 api.add_resource(PatientList, '/api/patients')
 api.add_resource(PatientData, '/api/patients/<int:patient_id>')
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
