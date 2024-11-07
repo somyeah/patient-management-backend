151bc6db12d9284401aae26cafa13eed3f8ed858
Somya Jain <sjain29@usfca.edu>
2024-11-06 17:18:51 -0800

    Modify CORS parameters

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_object(Config)
 api = Api(app)
 
 db.init_app(app)
-CORS(app)
+CORS(app, origins=["http://localhost:3000"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], allow_headers=["Content-Type"])
 
 with app.app_context():
     db.create_all()
