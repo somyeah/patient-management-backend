14f07a4e4775539fadbb6e62c1413ea267411e6d
Somya Jain <sjain29@usfca.edu>
2024-11-07 08:07:37 -0800

    CORS headers?

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_object(Config)
 api = Api(app)
 
 db.init_app(app)
-CORS(app)
+CORS(app, origins=["http://localhost:3000"], methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"], supports_credentials=True)
 
 with app.app_context():
     db.create_all()
